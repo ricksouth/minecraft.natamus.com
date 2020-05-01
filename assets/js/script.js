938c61e4693000b308c8e1933b83958e82ac15ed
Rick South <rick@natamus.com>
2020-05-01 16:55:53 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,7 +57,7 @@ function loadContent() {
 						filetype = "gif";
 					}
 
-					style += 'div#mod' + i + ':before { background: url("/assets/images/icons/' + slug + '.' + filetype + '"); background-position: center center; background-size: cover; content: "\f019 ' + moddata[slug]["downloadCount"]'" } div#mod' + i + ':after { content: "' + name + '"; }';
+					style += 'div#mod' + i + ':before { background: url("/assets/images/icons/' + slug + '.' + filetype + '"); background-position: center center; background-size: cover; content: "\f019 ' + moddata[slug]["downloadCount"] + '" } div#mod' + i + ':after { content: "' + name + '"; }';
 					
 					html += '<div class="col mod"><a href="' + url + '"></a><a href="' + url + '"></a><a href="' + url + '"></a><a href="' + url + '"></a><div id="mod' + i + '" class="box"></div></div>';
 					//html += '<div class="col mod"><a href="' + url + '"></a><a href="' + url + '"></a><a href="' + url + '"></a><a href="' + url + '"></a><div id="mod' + i + '" class="box"></div><img class="dlshield" src="https://cf.way2muchnoise.eu/' + slug + '.svg" alt="' + name + '"></div>';
