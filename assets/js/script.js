7959a6a788c807da0179e1dd01e86bad3a140c86
Rick South <rick@natamus.com>
2020-05-01 12:52:32 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,28 @@ function loadContent() {
 	$.ajax({
 		url: "https://raw.githubusercontent.com/ricksouth/serilum-mc-mods/master/README.md",
 		success: function(data){
+			var html = "";
+
 			var dataspl = data.split("\n");
 			for (var i = 0; i < dataspl.length; i++) {
 				var line = dataspl[i];
 				if (line.includes("/mc-mods/")) {
-					console.log(line);
+					var linespl = line.split("](");
+					if (linespl.length < 2) {
+						continue;
+					}
+
+					var name = linespl[0];
+					var url = linespl[1];
+
+					html += '<div class="mod"><a href="' + url + '">' + name + "</a></div>";
+				}
+				else if (line.includes("Discontinued")) {
+					break;
 				}
 			}
+
+			$("#content").html(html);
 		}
 	});
 }
