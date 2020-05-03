eb087b3a23f4fb1422003ec32ac10656189703a7
Rick South <rick@natamus.com>
2020-05-03 16:43:00 +0200

    Add sitemap (and generator script)

---
+++ sitemap-generator.py
@@ -0,0 +1,45 @@
+from datetime 							import datetime
+import urllib.request
+import pathlib
+import os
+
+def main():
+	localpath = str(pathlib.Path(__file__).parent)
+	url = "https://raw.githubusercontent.com/ricksouth/serilum-mc-mods/master/README.md"
+
+	now = datetime.today()
+	time = now.strftime('%Y%m%d%H%M%S')
+	lastmod = now.strftime('%Y-%m-%d')
+
+	with open(os.path.join(localpath,"sitemap.xml_" + time), 'w') as smfile:
+		smfile.write('<?xml version="1.0" encoding="UTF-8"?>' + "\n")
+		smfile.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' + "\n")
+
+		smfile.write('	<url>' + "\n")
+		smfile.write('		<loc>https://minecraft.natamus.com/</loc>' + "\n")
+		smfile.write('		<lastmod>' + lastmod + '</lastmod>' + "\n")
+		smfile.write('		<changefreq>daily</changefreq>' + "\n")
+		smfile.write('		<priority>1.00</priority>' + "\n")
+		smfile.write('	</url>' + "\n")
+
+		for decl in urllib.request.urlopen(url):
+			line = decl.decode('utf-8')
+			if "/mc-mods/" in line:
+				slug = line.split("/mc-mods/")[1].split(")")[0]
+
+				smfile.write('	<url>' + "\n")
+				smfile.write('		<loc>https://minecraft.natamus.com/' + slug + '</loc>' + "\n")
+				smfile.write('		<lastmod>' + lastmod + '</lastmod>' + "\n")
+				smfile.write('		<changefreq>weekly</changefreq>' + "\n")
+				smfile.write('		<priority>0.80</priority>' + "\n")
+				smfile.write('	</url>' + "\n")
+
+			if "Discontinued" in line:
+				break
+
+		smfile.write('</urlset>')
+
+	return
+
+if __name__ == '__main__':
+	main()
