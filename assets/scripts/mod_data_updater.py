cc460bfac343cc0f41a74a506bae448dec8f1b80
Rick South <38502394+ricksouth@users.noreply.github.com>
2021-09-04 18:45:18 +0200

    Revamp site. Update mod data.

--- assets/scripts/mod_data_updater.py
+++ assets/scripts/mod_data_updater.py
@@ -7,6 +7,7 @@ import os
 import json
 import shutil
 import demjson
+import io
 
 # Updates mod file information via the CurseForge API.
 # by Rick South.
@@ -16,6 +17,8 @@ def main():
 
 	# Copy README
 	shutil.copy("C:/The Forge/serilum-mc-mods/README.md", "C:/The Forge/minecraft.natamus.com/data/README.md")
+
+	slugs = []
 
 	# Copy mod data from CurseForge.
 	allglobaldata = "{"
@@ -30,6 +33,7 @@ def main():
 
 		modname = linespl[0]
 		slug = modname.lower().replace(" ", "-")
+		slugs.append(slug)
 		projectid = linespl[1]
 		print(modname, projectid)
 
@@ -58,13 +62,21 @@ def main():
 	print("allglobaldata: ", len(allglobaldata))
 	print("alldescriptions: ", len(alldescriptions))
 
-	with open('C:/The Forge/minecraft.natamus.com/data/mod_data.json', 'w') as allfile:
+	with io.open('C:/The Forge/minecraft.natamus.com/data/mod_data.json', 'w', encoding="utf-8") as allfile:
 		allfile.write(allglobaldata)
 
-	with open('C:/The Forge/minecraft.natamus.com/data/description_data.json', 'w') as descfile:
+	with io.open('C:/The Forge/minecraft.natamus.com/data/description_data.json', 'w', encoding="utf-8") as descfile:
 		descfile.write(demjson.encode(alldescriptions))
 
 	print("Mod data generated.")
+
+	print("\nCreating real html pages for scrapers.")
+	for slug in slugs:
+		os.makedirs("C:/The Forge/minecraft.natamus.com/" + slug, exist_ok=True)
+		os.makedirs("C:/The Forge/minecraft.natamus.com/" + slug + "/changelog", exist_ok=True)
+		shutil.copy("C:/The Forge/minecraft.natamus.com/index.html", "C:/The Forge/minecraft.natamus.com/" + slug + "/index.html")
+		shutil.copy("C:/The Forge/minecraft.natamus.com/index.html", "C:/The Forge/minecraft.natamus.com/" + slug + "/changelog/index.html")
+
 	return
 
 if __name__ == '__main__':
