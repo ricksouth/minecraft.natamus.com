90ffee0657c9b7e51cfdd1637e7abafec46b93d8
Rick South <rick@natamus.com>
2022-08-08 19:05:28 +0200

    Updated redirect function (again).

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 // https://minecraft.natamus.com/anvil-restoration/changelog/
 document.addEventListener('DOMContentLoaded', function() {
-	var url = document.URL;
+	var url = document.URL + "";
 
+	var newurl = "https://serilum.com/";
 	if (url.includes("/changelog/")) {
-		var newurl = "https://serilum.com/mods?changelog=" + url.split("natamus.com/")[1].replace("/changelog/", "");
-
-		window.location.replace(newurl);
-		return;
+		if (url.includes("/?p=/")) {
+			newurl = "https://serilum.com/mods?changelog=" + url.split("/?p=/")[1].split("/changelog/")[0];
+		}
+		else {
+			newurl = "https://serilum.com/mods?changelog=" + url.split("natamus.com/")[1].split("/changelog/")[0];
+		}
 	}
 
-	window.location.replace("https://serilum.com/");
+	window.location.replace(newurl);
 }, false);
